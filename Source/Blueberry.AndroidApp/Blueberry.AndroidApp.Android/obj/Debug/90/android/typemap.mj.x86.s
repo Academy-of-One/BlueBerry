f476ceae666441a47e8ffff85f929fd08c79cbae
Jack Jenkins <jackjenk1993@yahoo.com>
2019-08-25 03:55:35 -0500

    Added IOS and Android App
    Added IOS and Android ap

---
+++ Source/Blueberry.AndroidApp/Blueberry.AndroidApp.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 6e67e684c9d86127ee82416786c7efdd94ec8385 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1491
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
---
+++ Source/Blueberry.AndroidApp/Blueberry.AndroidApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Blueberry.AndroidApp.Models;
+
+namespace Blueberry.AndroidApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
